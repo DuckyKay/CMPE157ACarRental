97cf23bd38feea1c141be21508b17f3fd0a99855
GreenXDShadow <NickDValen@gmail.com>
2024-05-02 02:53:24 -0700

    Updates to Login and Sign Up

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-
-db = SQLAlchemy()
-DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'changeKeyLater' # encrypt cookies <-- we will have to hide this key locally
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # sqlite DB stored in website folder
-    db.init_app(app) 
 
     from .views import views
     from .auth import auth
@@ -17,19 +10,5 @@ def create_app():
     # Define prefix for route if you want
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
-    
-    # need to import ALL CLASSES
-    from .models import User, Note
-    
-    create_database(app)
 
     return app
-
-def create_database(app):
-    with app.app_context():
-        if not path.exists('instance/' + DB_NAME):
-            db.create_all()
-            print('Created Database!')
-        else:
-            print('Database already exists!')
-    
