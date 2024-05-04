5b52a609229b60464cdf14ccc6e54da64b0a1cd9
GreenXDShadow <NickDValen@gmail.com>
2024-05-03 20:18:21 -0700

    Login checks for user. Still need to implement sessions

--- website/auth.py
+++ website/auth.py
@@ -10,6 +10,19 @@ auth = Blueprint('auth', __name__)  # blueprint setup
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()  # search column for entered email and return first result
+        if user:
+            if check_password_hash(user.password, password):  # if password matches
+                flash('Logged in successfully!', 'success')
+            else:
+                flash('Incorrect password, try again', 'error')
+        else:
+            flash('Email does not exist', 'error')
+
     return render_template('login.html')
 
 
@@ -26,7 +39,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
