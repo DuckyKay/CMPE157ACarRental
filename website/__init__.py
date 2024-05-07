5fc3cc68977b6b2d6fc6e177b179b049da18b7ee
GreenXDShadow <NickDValen@gmail.com>
2024-05-07 01:06:40 -0700

    Okay reservations can now be edited

--- website/__init__.py
+++ website/__init__.py
@@ -1,23 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import login_manager, LoginManager
+from flask_login import LoginManager
+from datetime import datetime
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'changeKeyLater' # encrypt cookies <-- we will have to hide this key locally
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # sqlite DB stored in website folder
+    app.config['SECRET_KEY'] = 'changeKeyLater'  # encrypt cookies <-- we will have to hide this key locally
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  # sqlite DB stored in website folder
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     # Define prefix for route if you want
-    app.register_blueprint(views,url_prefix='/')
-    app.register_blueprint(auth,url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     # need to import ALL CLASSES
     from .models import User, Car, Location, Reservation
@@ -28,11 +30,16 @@ def create_app():
     login_manager.login_view = 'auth.login'  # Where flask redirects if user not logged in
     login_manager.init_app(app)
 
-    @login_manager.user_loader # Tells flask how we load a user
+    @login_manager.user_loader  # Tells flask how we load a user
     def load_user(id):
         return User.query.get(int(id))
 
+    @app.template_filter('datetime_local')
+    def datetime_local_filter(value):
+        return value.strftime('%Y-%m-%dT%H:%M')
+
     return app
+
 
 def create_database(app):
     with app.app_context():
